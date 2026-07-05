9eb456efa9b8e98887553eb1f577256a18aa4164
Cari UwU <luckpruevas@gmail.com>
2026-07-05 17:41:50 -0300

    arreglar conflictos

--- PericoBack/src/routes/reserva.routes.js
+++ PericoBack/src/routes/reserva.routes.js
@@ -5,11 +5,9 @@ const router = express.Router();
 
 router.get('/', reservaCtrl.obtenerReservas);
 router.post('/', reservaCtrl.registrarReserva);
-<<<<<<< HEAD
 router.post('/webhook', reservaCtrl.recibirNotificacionPago);
-=======
 router.patch('/:idReserva/estado', reservaCtrl.cambiarEstadoReserva);
 router.patch('/:idReserva/cancelar', reservaCtrl.cancelarReserva);
->>>>>>> origin/develop
+
 
 module.exports = router;

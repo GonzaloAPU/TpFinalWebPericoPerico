c202fa405ceb9c0935252e730eaa7e6964f48be4
Cari UwU <luckpruevas@gmail.com>
2026-07-06 00:27:26 -0300

    agregar endpoit modificar pasajero

--- PericoBack/src/routes/pasajero.routes.js
+++ PericoBack/src/routes/pasajero.routes.js
@@ -8,5 +8,6 @@ router.get('/:idPasajero/historial', pasajeroCtrl.obtenerHistorialPasajero);
 router.patch('/:idPasajero/ubicacion', pasajeroCtrl.actualizarUbicacionPasajero);
 router.get('/:idPasajero', pasajeroCtrl.obtenerPasajero);
 router.post('/', pasajeroCtrl.registrarPasajero);
+router.put('/:idPasajero', pasajeroCtrl.actualizarPasajero);
 
 module.exports = router;

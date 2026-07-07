e54b0ff1427d9c9ce4b80f3684b6d6e4679b29f4
GonzaloAPU <gonzaloc1704@gmail.com>
2026-07-06 23:40:33 -0300

    cambio de bandera

--- PericoBack/index.js
+++ PericoBack/index.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
   });
 });
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false }) // Cambiar a true si quieres reiniciar las tablas en cada inicio
   .then(() => {
     console.log('Tablas de PostgreSQL sincronizadas correctamente.');
     server.listen(app.get('port'), () => {

81d7ce8a085a923956ba16a8eb2967eb4d0c9cf4
Facundo897 <facundogaspar897@gmail.com>
2026-07-07 00:04:50 -0300

    ajustes en chofer controller

--- PericoBack/index.js
+++ PericoBack/index.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
   });
 });
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
   .then(() => {
     console.log('Tablas de PostgreSQL sincronizadas correctamente.');
     server.listen(app.get('port'), () => {
